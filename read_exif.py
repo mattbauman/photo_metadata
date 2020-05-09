2c59fb970df41df853cb0112c4d4ed8cf90e11b7
mattbauman <matt@mattbauman.com>
2020-05-09 10:42:24 -0500

    read_exif.py

--- read_exif.py
+++ read_exif.py
@@ -1,11 +1,8 @@
 import piexif
 from PIL import Image
 
-# exif_dict = piexif.load('C:/Users/matt/OneDrive/Pictures/2006-05 - Graduation Slide Show Pictures/Matt in A paper bag.jpg')
-# －DateTimeOrginal; ID= 36867 －DateTimeDigitized; ID= 36868 
-
-#exif_dict = piexif.load(r"C:\Users\matt\OneDrive\Pictures\2015-01-16\2014-01-10-Nickelodeon_Universe.jpg")
-exif_dict = piexif.load(r"C:\Users\matt\OneDrive\Desktop\photo_scanner\DingleFritz-Accnt_3.jpg")
+input_image = r"C:\Users\matt\OneDrive\Desktop\photo_scanner\DingleFritz-Accnt_3.jpg"
+exif_dict = piexif.load(input_image)
 
 for ifd_name in exif_dict:
     print("\n{0} IFD:".format(ifd_name))
